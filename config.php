56d1f90acd68731ef031696d1f7e1c770ebd0178
Naod <naodabeben@gmail.com>
2025-08-31 15:40:54 +0300

    1.3

--- config.php
+++ config.php
@@ -6,12 +6,12 @@ define('APPLICATION', 'Catalog');
 define('HTTP_SERVER', 'http://localhost/warka/');
 
 // DIR
-define('DIR_OPENCART', 'C:/xampp/htdocs/warka/');
+define('DIR_OPENCART', __DIR__ . '/');
 define('DIR_APPLICATION', DIR_OPENCART . 'catalog/');
 define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
 define('DIR_IMAGE', DIR_OPENCART . 'image/');
 define('DIR_SYSTEM', DIR_OPENCART . 'system/');
-define('DIR_STORAGE', 'C:/xampp/storage/');
+define('DIR_STORAGE', DIR_OPENCART . 'storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
