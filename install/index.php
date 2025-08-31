7bf0e84d43232ec7dd0463c3164971896be74b49
Naod <naodabeben@gmail.com>
2025-08-31 23:05:15 +0300

    1.11

--- install/index.php
+++ install/index.php
@@ -38,6 +38,8 @@ define('DIR_UPLOAD', DIR_SYSTEM . 'storage/upload/');
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
 
+// Composer autoloader (must come before framework.php)
+require_once __DIR__ . '/../vendor/autoload.php';
+
 // Framework
 require_once(DIR_SYSTEM . 'framework.php');
-require_once __DIR__ . '/../vendor/autoload.php';
